8785115abef9978241273c43fb5f73b4a82619d4
Rishav Yadav <rishavyadav154@gmail.com>
2024-10-26 12:21:41 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function init() {
+  document
+    .getElementById("dropdown-for-my-apps")
+    .addEventListener("click", (e) => {
+      showDropDown(e);
+    });
+  document
+    .getElementById("dropdown-for-user-profile")
+    .addEventListener("click", (e) => {
+      showDropDown(e);
+    });
+  const scrollsDown = document.querySelectorAll(".scroll-down");
+  scrollsDown.forEach((i) => {
+    i.addEventListener("click", (e) => {
+      let target = e.target;
+      if (target.tagName === "I") {
+        console.log("its I");
+        target = target.parentElement;
+      }
+      target = target.parentElement;
+      target
+        .querySelector(".scrollable-lists")
+        .scrollBy({ top: 100, behaviour: "smooth" });
+    });
+  });
+
+  const scrollsRight = document.querySelectorAll(".scroll-right");
+  scrollsRight.forEach((i) => {
+    i.addEventListener("click", (e) => {
+      console.log("right ran");
+      let target = e.target;
+      if (target.tagName === "I") {
+        console.log("its I");
+        target = target.parentElement;
+      }
+      target = target.parentElement;
+      target
+        .querySelector(".x-scrollable-lists")
+        .scrollBy({ left: 100, behaviour: "smooth" });
+    });
+  });
+}
+
+function showDropDown(e) {
+  let target = e.target;
+  if (target.tagName === "BUTTON") {
+    target = e.target.parentElement;
+  }
+  if (target.tagName === "I") {
+    target = e.target.parentElement.parentElement;
+  }
+
+  target.querySelector("ul").classList.toggle("show");
+}
+
+window.addEventListener("DOMContentLoaded", init);
